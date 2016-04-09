e6b02a33e8821ef66b243f1f23d7d46a539a4031
ray321 <ravenconyers321@gmail.com>
2016-04-09 19:07:14 +0000

    Refactor to use MVC-style routing

---
+++ apples.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+   res.send('Hello World!');
+});
+
+app.get('/hello', function(req, res) {
+    res.send('Well, hello to you too!');
+});
+
+app.get('/bye', function(req, res) {
+    res.send('I hope to see you again!');
+});
+
+app.listen(process.env.PORT, function () {
+    console.log('Example app listening on port 3000!');
+});
+    
+
+
---
+++ controllers/index.js
@@ -0,0 +1,8 @@
+// Local dependency 
+var Home = require('./home');
+var Users = require('./users');
+
+module.exports = {
+    Home: Home,
+    Users: Users
+};
